bf8093538ae7c0aad0a448eef4fc0ac7297d2a55
Umputun <umputun@gmail.com>
2016-01-11 00:26:21 -0600

    vendored tests removed

--- extractor/readabilty_test.go
+++ extractor/readabilty_test.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"net/url"
+	"strings"
 	"testing"
 	"time"
 
@@ -12,6 +13,7 @@ import (
 
 	"umputun.com/ukeeper/ureadability/datastore"
 
+	"github.com/PuerkitoBio/goquery"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -64,6 +66,20 @@ func TestExtractPics(t *testing.T) {
 	assert.Equal(t, "http://p.umputun.com/content/images/2015/09/n891a_20150925_023343-minwz.png", a.AllImages[0])
 	assert.Equal(t, "http://p.umputun.com/content/images/2015/09/ios9nav-heavytraffic-6c-1.jpg", a.AllImages[1])
 	assert.Equal(t, "http://p.umputun.com/content/images/2015/09/apple-maps-app-220-100.jpg", a.AllImages[2])
+}
+
+func TestExtracPicsDirectly(t *testing.T) {
+	t.Log("test pic directly")
+	data := `<body>
+		<img class="alignright size-full wp-image-944214 lazyloadableImage lazyLoad-fadeIn" alt="View Page Source" width="308" height="508" data-original="http://cdn1.tnwcdn.com/wp-content/blogs.dir/1/files/2016/01/page-source.jpg" src="http://cdn1.tnwcdn.com/wp-content/blogs.dir/1/files/2016/01/page-source.jpg"></body>`
+	d, err := goquery.NewDocumentFromReader(strings.NewReader(data))
+	assert.Nil(t, err)
+	sel := d.Find("img")
+	lr := UReadability{TimeOut: 30, SnippetSize: 200}
+	im, allImgs, ok := lr.extractPics(sel, "url")
+	assert.True(t, ok)
+	assert.Equal(t, 1, len(allImgs))
+	assert.Equal(t, "http://cdn1.tnwcdn.com/wp-content/blogs.dir/1/files/2016/01/page-source.jpg", im)
 }
 
 func TestNormilizeLinks(t *testing.T) {
