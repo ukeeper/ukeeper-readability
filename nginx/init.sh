24ef66e0c4f1545e88435735021785781622f29f
Umputun <umputun@gmail.com>
2016-01-16 22:03:49 -0600

    compose with web nginx and mongo

---
+++ nginx/init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "set auth users"
+echo $NGINX_ADMIN > /etc/nginx/users
+echo $NGINX_TEST >> /etc/nginx/users
+echo >> /etc/nginx/users
+cat /etc/nginx/users
+
+exec nginx -g "daemon off;"
