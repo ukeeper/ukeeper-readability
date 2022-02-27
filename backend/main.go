6621f5be610817ff87eac64dacae97c5f8c7c049
Dmitry Verkhoturov <paskal.07@gmail.com>
2022-02-27 11:22:19 +0100

    replace mongo params with single connection URI

--- backend/main.go
+++ backend/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"os"
+	"time"
 
 	log "github.com/go-pkgz/lgr"
 	"github.com/jessevdk/go-flags"
@@ -14,11 +15,9 @@ import (
 var revision string
 
 var opts struct {
-	Mongo       string            `short:"m" long:"mongo" env:"MONGO" description:"mongo host:port"`
-	MongoPasswd string            `short:"p" long:"mongo-password" env:"MONGO_PASSWD" default:"" description:"mongo pssword"`
-	MongoDelay  int               `long:"mongo-delay" env:"MONGO_DELAY" default:"0" description:"mongo initial delay"`
+	MongoURI    string            `short:"m" long:"mongo_uri" env:"MONGO_URI" required:"true" description:"MongoDB connection string"`
+	MongoDelay  time.Duration     `long:"mongo-delay" env:"MONGO_DELAY" default:"0" description:"mongo initial delay"`
 	MongoDB     string            `long:"mongo-db" env:"MONGO_DB" default:"ureadability" description:"mongo database name"`
-	Migrate     bool              `long:"migrate" description:"enable migration"`
 	Credentials map[string]string `long:"creds" env:"CREDS" description:"credentials for protected calls"`
 	Debug       bool              `long:"dbg" env:"DEBUG" description:"debug mode"`
 }
@@ -30,9 +29,12 @@ func main() {
 	setupLog(opts.Debug)
 
 	log.Printf("[INFO] started ureadability service, %s", revision)
-	rules := datastore.New(opts.Mongo, opts.MongoPasswd, opts.MongoDB, opts.MongoDelay).GetStores()
+	db, err := datastore.New(opts.MongoURI, opts.MongoDB, opts.MongoDelay)
+	if err != nil {
+		log.Fatalf("[ERROR] can't connect to mongo %v", err)
+	}
 	srv := rest.Server{
-		Readability: extractor.UReadability{TimeOut: 30, SnippetSize: 300, Rules: rules, Debug: opts.Debug},
+		Readability: extractor.UReadability{TimeOut: 30, SnippetSize: 300, Rules: db.GetStores(), Debug: opts.Debug},
 		Credentials: opts.Credentials,
 		Version:     revision,
 	}
