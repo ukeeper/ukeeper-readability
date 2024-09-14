f77739bf79fad1fa06cb305b00889d6ce5abd836
Dmitry Verkhoturov <paskal.07@gmail.com>
2024-09-15 00:22:29 +0100

    Switch the main page to HTMX

--- backend/main.go
+++ backend/main.go
@@ -20,7 +20,7 @@ var revision string
 var opts struct {
 	Address     string            `long:"address" env:"UKEEPER_ADDRESS" default:"" description:"listening address"`
 	Port        int               `long:"port" env:"UKEEPER_PORT" default:"8080" description:"port"`
-	FrontendDir string            `long:"frontend_dir" env:"FRONTEND_DIR" default:"/srv/web" description:"directory with frontend files"`
+	FrontendDir string            `long:"frontend_dir" env:"FRONTEND_DIR" default:"/srv/web" description:"directory with frontend templates and static/ directory for static assets"`
 	Credentials map[string]string `long:"creds" env:"CREDS" description:"credentials for protected calls (POST, DELETE /rules)"`
 	Token       string            `long:"token" env:"UKEEPER_TOKEN" description:"token for /content/v1/parser endpoint auth"`
 	MongoURI    string            `short:"m" long:"mongo_uri" env:"MONGO_URI" required:"true" description:"MongoDB connection string"`
